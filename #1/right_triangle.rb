86046240fdb6b1531944e90acdca034a7d99f036
vdashman <v.nurmatov@ipvideos.ru>
2021-01-31 09:35:28 +0300

    Fix Lesson #1

--- #1/right_triangle.rb
+++ #1/right_triangle.rb
@@ -7,17 +7,15 @@ b_side = gets.to_f
 puts "Введите сторону C треугольника:"
 c_side = gets.to_f
 
-sides = [a_side, b_side, c_side].sort
-# hypo = sides.max
-#puts "Гипотенуза равна #{hypo}"
+side_1, side_2, hypo = [a_side, b_side, c_side].sort
 
-if ( sides[0] ** 2 + sides[1] ** 2 ) == ( sides[2] ** 2 )
+if (side_1**2 + side_2**2) == (hypo**2)
   puts "Ваш треугольник прямоугольный"
-elsif ( sides[0] ** 2 + sides[1] ** 2 ) == ( sides[2] ** 2 ) && ( sides[0] == sides[1] && sides[1] != sides[2] )
+elsif (side_1**2 + side_2**2) == (hypo**2) && (side_1 == side_2 && side_2 != hypo)
   puts "Ваш треугольник прямоугольный и равнобедренный"
-elsif sides[0] == sides[1] && sides[1] == sides[2]
+elsif side_1 == side_2 && side_2 == hypo
   puts "Ваш треугольник равносторонний"
-elsif sides[0] == sides[1] && sides[1] != sides[2]
+elsif side_1 == side_2 && side_2 != hypo
   puts "Ваш треугольник равнобедренный"
 else
   puts "Ваш треугольник обычный"
