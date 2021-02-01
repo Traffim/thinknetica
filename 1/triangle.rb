2c27dc703daa4f3e173c0cc9cc3232255cf58b59
vdashman <v.nurmatov@ipvideos.ru>
2021-02-01 21:20:26 +0300

    Banned?

--- 1/triangle.rb
+++ 1/triangle.rb
@@ -2,6 +2,6 @@ puts "Введите длину основания треугольника:"
 triangle_base = gets.to_f
 puts "Введите высоту треугольника:"
 triangle_height = gets.to_f
-s = triangle_base * triangle_height / 2
+s = triangle_base * triangle_height/2
 
 puts "Площадь треугольника равна #{s} введенных едениц в кв."
